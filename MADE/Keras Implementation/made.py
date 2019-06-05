c49ba264ee0a5c2efbfde2909bf71bbcd9a69fea
Emma Alexandrov <ealexandrov@vassar.edu>
2019-06-05 11:08:22 -0400

    Reorganization
    Accounting for the addition of a new implementation of MADE.

---
+++ MADE/Keras Implementation/made.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# for now, we'll use MNIST as our dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+print(x_train.shape)
+
+# binarize data (the MADE architecture is not equipped to process continuous
+#   data)
+
+
+
+# do one-hot encoding
+
+# create new layer for masks
+
+# make network
+
+# train model
+
+# visualize training
