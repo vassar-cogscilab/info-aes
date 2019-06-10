0f4eea178155b5db96a868c2431b3ab21137e158
Emma Alexandrov <ealexandrov@vassar.edu>
2019-06-10 11:28:00 -0400

    Working MADE without direct input-output connections

--- MADE/KerasImplementation/layers.py
+++ MADE/KerasImplementation/layers.py
@@ -24,33 +24,19 @@ import numpy as np
 class MaskedDense(Layer):
     # these are parameters that can be passed in to the creation of a new instance, with their default values
     def __init__(self, units,
-                 data_features,
-                 input_indices=[],
-                 use_bias=True,
-                 kernel_initializer='glorot_uniform',
-                 bias_initializer='zeros',
-                 kernel_regularizer=None,
-                 bias_regularizer=None,
-                 activity_regularizer=None,
-                 kernel_constraint=None,
-                 bias_constraint=None, **kwargs):
+                 mask,
+                 # first_layer,
+                 **kwargs):
         if 'input_shape' not in kwargs and 'input_dim' in kwargs:
             kwargs['input_shape'] = (kwargs.pop('input_dim'),)
 
         super(MaskedDense, self).__init__(**kwargs)
         # each instance of the class will have all of these attributes
-        self.data_features = data_features
-        self.units = units
-        self.input_indices = input_indices
+        
+        # self.first_layer = first_layer
+        self.mask = mask
         self.activation = activations.get('relu')
-        self.use_bias = use_bias
-        self.kernel_initializer = initializers.get(kernel_initializer)
-        self.bias_initializer = initializers.get(bias_initializer)
-        self.kernel_regularizer = regularizers.get(kernel_regularizer)
-        self.bias_regularizer = regularizers.get(bias_regularizer)
-        self.kernel_constraint = constraints.get(kernel_constraint)
-        self.bias_constraint = constraints.get(bias_constraint)
-
+        self.units = units
         self.input_spec = InputSpec(min_ndim=2)
 
     def build(self, input_shape, ):
@@ -60,64 +46,25 @@ class MaskedDense(Layer):
         # note here how the weights matrix dimensions are equal to those of the
         #   mask
         self.kernel = self.add_weight(shape=(input_dim, self.units),
-                                      initializer=self.kernel_initializer,
-                                      name='kernel',
-                                      regularizer=self.kernel_regularizer,
-                                      constraint=self.kernel_constraint)
+                                      initializer=initializers.get('glorot_uniform'),
+                                      name='kernel')
 
-        def gen_mask(data_features, units, inputs, input_dim):
-            mask = np.zeros((input_dim, units), dtype=np.float32)
+        # bias is a vector, hence the shape argument here
+        self.bias = self.add_weight(shape=(self.units,),
+                                    initializer=initializers.get('zeros'),
+                                    name='bias')
 
-            # if they are provided in the layer input, get previous layer node
-            #   indices
-            input_indices = []
-            if type(inputs) == "class 'dict'":
-                input_indices = inputs['indices']
-
-            # if input indices are not supplied, we must generate a list
-            if len(input_indices) == 0:
-                for i in range(input_dim):
-                    input_indices.append(i+1)
-
-            # we must generate a list of node indices for this layer
-            layer_indices = []
-            for i in range(units):
-                layer_indices.append(np.random.randint(1, data_features - 1))
-
-            # populate mask with appropriate values
-            for i in range(len(input_indices)): # iterate over every layer node
-                for j in range(len(layer_indices)): # iterate over every input node
-                    if input_indices[i] <= layer_indices[j]:
-                        mask[i][j] = 1
-            
-            mask = tf.convert_to_tensor(mask, dtype=tf.float32)
-            return (mask, layer_indices)
-
-        mask, layer_indices = gen_mask(self.data_features, self.units, self.input_indices, input_dim)
-        self.mask = mask
-        self.layer_indices = layer_indices
-
-        if self.use_bias:
-            # bias is a vector, hence the shape argument here
-            self.bias = self.add_weight(shape=(self.units,),
-                                        initializer=self.bias_initializer,
-                                        name='bias',
-                                        regularizer=self.bias_regularizer,
-                                        constraint=self.bias_constraint)
-        else:
-            self.bias = None
         self.input_spec = InputSpec(min_ndim=2, axes={-1: input_dim})
-        self.built = True
+        super(MaskedDense, self).build(input_shape)
 
     # this function implements the layer's computations
     def call(self, inputs):
-        # assume that the input is not a dictionary (this is the case only for
-        #   the first hidden layer
+        """if self.first_layer:
+            self.first_input = inputs
+        else: 
+            # get first input from layer inputs"""
         hadamard_product = tf.multiply(self.mask, self.kernel)
-        input = inputs
-        if type(inputs) == "class 'dict'":
-            input = inputs['output']
-        input = ops.convert_to_tensor(input)
+        input = ops.convert_to_tensor(inputs)
         rank = common_shapes.rank(input)
         if rank > 2:
             dot_product = standard_ops.tensordot(input, hadamard_product, axes=[[rank - 1], [0]])
@@ -130,18 +77,11 @@ class MaskedDense(Layer):
             dot_product = gen_math_ops.mat_mul(input, hadamard_product)
         # multiply the inputs by the hadamard (elementwise) product of the
         #   mask matrix and the weights matrix
-        if self.use_bias:
-            output = K.bias_add(dot_product, self.bias, data_format='channels_last')
-        else:
-            output = dot_product
+        output = K.bias_add(dot_product, self.bias, data_format='channels_last')
         # pass the result of the layer through the activ. function
-        if self.activation is not None:
-            output = self.activation(output)
-        # implementing multiple outputs
-        outputs = {'output': output, 'indices': self.layer_indices}
-        return outputs
+        output = self.activation(output)
+        return output
 
-    # not sure what's goin on here but i know it's important
     def compute_output_shape(self, input_shape):
         assert input_shape and len(input_shape) >= 2
         assert input_shape[-1]
@@ -149,117 +89,134 @@ class MaskedDense(Layer):
         output_shape[-1] = self.units
         return tuple(output_shape)
 
-class MADEOutput(Layer):
+class TempOutput(Layer):
     # these are parameters that can be passed in to the creation of a new instance, with their default values
     def __init__(self, units,
-                 data_features,
-                 input_indices=[],
-                 activation=None,
-                 use_bias=True,
-                 kernel_initializer='glorot_uniform',
-                 bias_initializer='zeros',
-                 kernel_regularizer=None,
-                 bias_regularizer=None,
-                 activity_regularizer=None,
-                 kernel_constraint=None,
-                 bias_constraint=None,
+                 mask,
+                 # first_layer,
                  **kwargs):
         if 'input_shape' not in kwargs and 'input_dim' in kwargs:
             kwargs['input_shape'] = (kwargs.pop('input_dim'),)
-        super(MaskedDense, self).__init__(**kwargs)
 
+        super(TempOutput, self).__init__(**kwargs)
         # each instance of the class will have all of these attributes
-
-        self.data_features = data_features
-        self.units = units
-        self.input_indices = input_indices
-        self.activation = activations.get(activation)
-        self.use_bias = use_bias
-        self.kernel_initializer = initializers.get(kernel_initializer)
-        self.bias_initializer = initializers.get(bias_initializer)
-        self.kernel_regularizer = regularizers.get(kernel_regularizer)
-        self.bias_regularizer = regularizers.get(bias_regularizer)
-        self.kernel_constraint = constraints.get(kernel_constraint)
-        self.bias_constraint = constraints.get(bias_constraint)
-        self.input_spec = InputSpec(min_ndim=2)
-
-        assert len(input_shape) >= 2
-        input_dim = input_shape[-1]
-
-        def gen_mask(data_features, units, is_output, inputs, input_dim):
-            mask = np.zeros((units, input_dim))
-
-            # get previous layer node indices
-            input_indices = []
-            if type(inputs) == "class 'dict'":
-                input_indices = inputs['indices']
-
-            # we must generate a list of node indices for this layer
-            layer_indices = []
-            # if the layer is output, these indices should list the nodes in
-            #   order
-            for i in range(units):
-                layer_indices.append(i+1)
-
-            # populate mask with appropriate values
-            for i in range(len(layer_indices)):  # iterate over every layer node
-                for j in range(len(input_indices)):  # iterate over every input node
-                    if input_indices[j] < layer_indices[i]:  # if the input index > the index
-                        mask[i][j] = 1
-            return (mask, layer_indices)
-
-        mask, layer_indices = gen_mask(data_features, units, input_indices, input_dim)
+        
+        # self.first_layer = first_layer
         self.mask = mask
-        self.layer_indices = layer_indices
+        self.activation = activations.get('sigmoid')
+        self.units = units
+        self.input_spec = InputSpec(min_ndim=2)
 
     def build(self, input_shape, ):
         assert len(input_shape) >= 2
-        input_dim = input_shape[-1]
+        input_dim = input_shape[-1] # final elt. of input shape--disregard batch
 
         # note here how the weights matrix dimensions are equal to those of the
         #   mask
         self.kernel = self.add_weight(shape=(input_dim, self.units),
-                                      initializer=self.kernel_initializer,
-                                      name='kernel',
-                                      regularizer=self.kernel_regularizer,
-                                      constraint=self.kernel_constraint)
-        if self.use_bias:
-            # bias is a vector, hence the shape argument here
-            self.bias = self.add_weight(shape=(self.units,),
-                                        initializer=self.bias_initializer,
-                                        name='bias',
-                                        regularizer=self.bias_regularizer,
-                                        constraint=self.bias_constraint)
-        else:
-            self.bias = None
+                                      initializer=initializers.get('glorot_uniform'),
+                                      name='kernel')
+
+        # bias is a vector, hence the shape argument here
+        self.bias = self.add_weight(shape=(self.units,),
+                                    initializer=initializers.get('zeros'),
+                                    name='bias')
+
         self.input_spec = InputSpec(min_ndim=2, axes={-1: input_dim})
-        self.built = True
+        super(TempOutput, self).build(input_shape)
 
     # this function implements the layer's computations
-    # MULTIN inputs is an argument to the call() function––try implementing
-    #   multiple inputs here
     def call(self, inputs):
-        # assume that the input is not a dictionary (this is the case only for
-        #   the first hidden layer
-        input = inputs
-        if type(inputs) == "class 'dict'":
-            input = inputs['output']
+        """if self.first_layer:
+            self.first_input = inputs
+        else: 
+            # get first input from layer inputs"""
+        hadamard_product = tf.multiply(self.mask, self.kernel)
+        input = ops.convert_to_tensor(inputs)
+        rank = common_shapes.rank(input)
+        if rank > 2:
+            dot_product = standard_ops.tensordot(input, hadamard_product, axes=[[rank - 1], [0]])
+            shape = input.get_shape().as_list()
+            output_shape = shape[:-1] + [self.units]
+            dot_product.set_shape(output_shape)
+        else: 
+            if not self._mixed_precision_policy.should_cast_variables:
+                input = math_ops.cast(inputs, self.dtype)
+            dot_product = gen_math_ops.mat_mul(input, hadamard_product)
         # multiply the inputs by the hadamard (elementwise) product of the
         #   mask matrix and the weights matrix
-        output = tf.dot(input, tf.multiply(self.mask, self.kernel))
-        if self.use_bias:
-            output = tf.bias_add(output, self.bias, data_format='channels_last')
+        output = K.bias_add(dot_product, self.bias, data_format='channels_last')
         # pass the result of the layer through the activ. function
-        if self.activation is not None:
-            output = self.activation(output)
-        # implementing multiple outputs
-        outputs = {'output': output, 'indices': self.layer_indices}
-        return outputs
+        output = self.activation(output)
+        return output
 
-    # not sure what's goin on here but i know it's important
     def compute_output_shape(self, input_shape):
         assert input_shape and len(input_shape) >= 2
         assert input_shape[-1]
         output_shape = list(input_shape)
         output_shape[-1] = self.units
         return tuple(output_shape)
+
+"""class MADEOutput(Layer):
+    def __init__(self, units, 
+                 output_mask,
+                 direct_mask, 
+                 **kwargs):
+        if 'input_shape' not in kwargs and 'input_dim' in kwargs:
+            kwargs['input_shape'] = (kwargs.pop('input_dim'),)
+
+        super(MaskedDense, self).__init__(**kwargs)
+        
+        self.output_mask = output_mask
+        self.direct_mask = direct_mask
+        self.activation = activations.get('sigmoid')
+        self.units = units
+        self.input_spec = InputSpec(min_ndim=2)
+
+    def build(self, input_shape, ):
+        assert len(input_shape) >= 2
+        input_dim = input_shape[-1] 
+
+        self.kernel = self.add_weight(shape=(input_dim, self.units),
+                                      initializer=initializers.get('glorot_uniform'),
+                                      name='kernel')
+
+        self.kernel_direct = self.add_weight(shape=(self.units, self.units),
+                                      initializer=initializers.get('glorot_uniform'),
+                                      name='kernel')
+
+        self.bias = self.add_weight(shape=(self.units,),
+                                    initializer=initializers.get('zeros'),
+                                    name='bias')
+
+        self.input_spec = InputSpec(min_ndim=2, axes={-1: input_dim})
+        self.built = True
+
+    def call(self, inputs):
+        hadamard_product = tf.multiply(self.output_mask, self.kernel)
+        hadamard_product_direct = tf.multiply(self.direct_mask, self.kernel_direct)
+        input = ops.convert_to_tensor(input)
+        rank = common_shapes.rank(input)
+        if rank > 2:
+            dot_product = standard_ops.tensordot(input, hadamard_product, axes=[[rank - 1], [0]])
+            dot_product_direct = standard_ops.tensordot(input, hadamard_product_direct, axes=[[rank - 1], [0]])
+            shape = input.get_shape().as_list()
+            output_shape = shape[:-1] + [self.units]
+            dot_product.set_shape(output_shape)
+            dot_product_direct.set_shape(output_shape)
+        else: 
+            if not self._mixed_precision_policy.should_cast_variables:
+                input = math_ops.cast(inputs, self.dtype)
+            dot_product = gen_math_ops.mat_mul(input, hadamard_product)
+            dot_product_direct = gen_math_ops.mat_mul(input, hadamard_product_direct)
+        dot_product_total = gen_math_ops.Add(dot_product, dot_product_direct)
+        output = K.bias_add(dot_product_total, self.bias, data_format='channels_last')
+        output = self.activation(output)
+        return output
+
+    def compute_output_shape(self, input_shape):
+        assert input_shape and len(input_shape) >= 2
+        assert input_shape[-1]
+        output_shape = list(input_shape)
+        output_shape[-1] = self.units
+        return tuple(output_shape)"""
--- MADE/KerasImplementation/made.py
+++ MADE/KerasImplementation/made.py
@@ -3,35 +3,68 @@ from tensorflow.keras import backend as K
 from tensorflow.keras.layers import Layer
 from tensorflow.keras import Model
 from MADE.KerasImplementation.layers import MaskedDense
+from MADE.KerasImplementation.layers import TempOutput
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
-"""# for now, we'll use MNIST as our dataset
+# for now, we'll use MNIST as our dataset
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-# define binarize function for rank 1 tensors
-def binarize(x):
-    for i in range(x.shape[0]):
-        for j in range(x.shape[1]):
-                if x[i][j] > 0:
-                    x[i][j] = 1
-    return x
-
-# binarize data (the MADE architecture is not equipped to process continuous
-#   data)
-for i in range(x_train.shape[0]): # iterate through every training pixel:
-    x_train[i] = binarize(x_train[i])
-    if i < x_test.shape[0]: # binarize the test data, too
-        x_test[i] = binarize(x_test[i])
-
-# do one-hot encoding
-y_train = tf.keras.utils.to_categorical(y_train, num_classes=10)
-y_test = tf.keras.utils.to_categorical(y_test, num_classes=10)"""
+x_test = np.digitize(x_test, [0.0, 128]) - 1
+x_train = np.digitize(x_train, [0.0, 128]) - 1
 
 # network parameters
-batch_size = 256
-hidden_units = 50
+batch_size = 100
+hidden_layers = 5
+hidden_units = 150
 features = 784
+
+# generate an array of mask matrices
+def gen_masks(features, hidden_layers, hidden_units):
+    masks = []
+    first_input_indices = []
+    input_indices = []
+    input_dim = hidden_units
+    # generate a mask for each hidden layer
+    for i in range(hidden_layers):
+        if len(input_indices) == 0:
+            input_dim = features
+            for j in range(features):
+                first_input_indices.append(j+1)
+            input_indices = first_input_indices
+        # we must generate a list of node indices for this layer
+        layer_indices = []
+        for j in range(hidden_units):
+            layer_indices.append(np.random.randint(1, features - 1))
+        mask = np.zeros((input_dim, hidden_units), dtype=np.float32)
+        # populate mask with appropriate values
+        for j in range(len(input_indices)): # iterate over every layer node
+            for k in range(len(layer_indices)): # iterate over every input node
+                if input_indices[j] <= layer_indices[k]:
+                    mask[j][k] = 1
+        mask = tf.convert_to_tensor(mask, dtype=tf.float32)
+        masks.append(mask)
+        input_indices = layer_indices
+        input_dim = len(input_indices)
+    # generate output layer masks
+    output_mask = np.zeros((hidden_units, features), dtype=np.float32)
+    for j in range(len(input_indices)): # iterate over every layer node
+        for k in range(len(first_input_indices)): # iterate over every input node
+            if input_indices[j] < first_input_indices[k]:
+                output_mask[j][k] = 1
+    output_mask = tf.convert_to_tensor(output_mask, dtype=tf.float32)
+    masks.append(output_mask)
+    direct_mask = np.zeros((features, features), dtype=np.float32)
+    for j in range(len(first_input_indices)): # iterate over every layer node
+        for k in range(len(first_input_indices)): # iterate over every input node
+            if first_input_indices[j] <= first_input_indices[k]:
+                direct_mask[j][k] = 1
+    direct_mask = tf.convert_to_tensor(direct_mask, dtype=tf.float32)
+    masks.append(direct_mask)
+    return masks
+
+masks = gen_masks(features, hidden_layers, hidden_units)
 
 # NOTE ON LAYER OUTPUTS AND INPUTS. Because the masked layers require multiple
 #   inputs (specifically, the previous layers' output matrix and node index
@@ -40,10 +73,12 @@ features = 784
 # make network
 inputs = tf.keras.Input(shape=(28,28))
 flatten = tf.keras.layers.Flatten()(inputs) # flatten matrix data to vectors
-h_1 = MaskedDense(hidden_units, features)(flatten) #
-h_2 = MaskedDense(hidden_units, features)(h_1)
-h_3 = MaskedDense(hidden_units, features)(h_2)
-outputs = tf.keras.layers.Dense(784, activation='sigmoid')(h_3)
+h_1 = MaskedDense(hidden_units, masks[0])(flatten) 
+h_2 = MaskedDense(hidden_units, masks[1])(h_1)
+h_3 = MaskedDense(hidden_units, masks[2])(h_2)
+h_4 = MaskedDense(hidden_units, masks[3])(h_3)
+h_5 = MaskedDense(hidden_units, masks[4])(h_4)
+outputs = TempOutput(784, masks[5])(h_5)
 unflatten = tf.keras.layers.Reshape((28,28))(outputs)
 made = Model(inputs=inputs, outputs=unflatten)
 made.summary()
@@ -52,7 +87,7 @@ made.compile(optimizer=tf.keras.optimizers.Adam(0.001),
     metrics=['accuracy'])
 
 # train model
-history=made.fit(x=x_train,y=x_train,batch_size=batch_size,epochs=20)
+history=made.fit(x=x_train,y=x_train,batch_size=batch_size,epochs=60,verbose=0)
 
 # visualize training
 plt.plot(history.history['accuracy'])
@@ -68,17 +103,96 @@ plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.show()
 
-# visualize auto-encoding capacity
-choice = x_test[np.random.randint(0,x_test.shape[0]+1)]
-input = np.empty((1,28,28))
-input[0] = choice
-output = made.predict(input,batch_size=1)
-result = output[0]
+# visualize auto-encoding and generative capacities
 
-plt.figure(figsize=(5,5))
-plt.imshow(choice,cmap='gray')
-plt.show()
+# auto-encoding
+def auto_encode(num_samples):
+    # visualize inputs
+    original_images = plt.figure(figsize=(5,5), facecolor='#ffffff')
+    # visualize auto-encodings
+    output_images = plt.figure(figsize=(5,5), facecolor='#ffffff')
+    for i in range(num_samples):
+        proto_input = x_test[np.random.randint(0,x_test.shape[0]+1)]
+        subplot = original_images.add_subplot(math.sqrt(num_samples), math.sqrt(num_samples), i+1)
+        subplot.imshow(proto_input,cmap='gray')
+        subplot.axis('off')
+        input = np.empty((1,28,28))
+        input[0] = proto_input
+        output = made.predict(input,batch_size=1)
+        subplot = output_images.add_subplot(math.sqrt(num_samples), math.sqrt(num_samples), i+1)
+        subplot.imshow(output[0],cmap='gray')
+        subplot.axis('off')
+    plt.show()
+    
 
-plt.figure(figsize=(5,5))
-plt.imshow(result,cmap='gray')
-plt.show()
+# novel sample generation
+def zoom_generation(start, end, step):
+    plots = math.ceil(((end - start) / step) + 1)
+    plot_size = math.ceil(math.sqrt(plots))
+    noise = np.random.binomial(1,0.5,size=(1,28,28))
+    row_length = noise.shape[1]
+    input = noise[0]
+    history_image = plt.figure(figsize=(10,10), facecolor='#ffffff')
+    subplot = history_image.add_subplot(plot_size, plot_size, 1)
+    subplot.imshow(input,cmap='gray')
+    subplot.axis('off')
+    for i in range(start, end + 1): 
+        output = made.predict(noise,batch_size=1)
+        p = output[0][i//row_length][i%row_length]
+        sample = np.random.binomial(1, p)
+        output[0][i//row_length][i%row_length] = sample
+        if i % step == 0:
+            plot_number = ((end - i )// step) + 1
+            subplot = history_image.add_subplot(plot_size, plot_size, plot_number)
+            subplot.imshow(output[0],cmap='gray')
+            subplot.axis('off')
+    plt.show()
+
+def generate_sample_with_history(noise_parameter):
+    noise = np.random.binomial(1,noise_parameter,size=(1,28,28))
+    row_length = noise.shape[1]
+    input = noise[0]
+    plt.figure(figsize=(5,5))
+    plt.imshow(input,cmap='gray')
+    plt.show()
+
+    output = np.zeros(input.shape, dtype=np.float32)
+    history_image = plt.figure(figsize=(8,8), facecolor='#ffffff')
+    for i in range(1, len(input.flatten())): 
+        noise = made.predict(noise,batch_size=1)
+        p = noise[0][i//row_length][i%row_length]
+        sample = np.random.binomial(1, p)
+        output[i//row_length][i%row_length] = sample
+        if i % 16 == 0:
+            plot_number = i // 16 + 1
+            subplot = history_image.add_subplot(7, 7, plot_number)
+            subplot.imshow(output,cmap='gray')
+            subplot.axis('off')
+    plt.show()
+
+    #visualize output
+    plt.figure(figsize=(5,5))
+    plt.imshow(output,cmap='gray')
+    plt.show()
+
+def generate_samples(num_samples):
+    noise_parameter = np.random.rand()
+    plot_size = math.ceil(math.sqrt(num_samples))
+    generated_samples = plt.figure(figsize=(10,10), facecolor='#ffffff')
+    for i in range(num_samples):
+        output = np.zeros(input.shape, dtype=np.float32)
+        noise = np.random.binomial(1,noise_parameter,size=(1,28,28))
+        row_length = noise.shape[1]
+        for j in range(1, len(input.flatten())): 
+            noise = made.predict(noise,batch_size=1)
+            p = noise[0][j//row_length][j%row_length]
+            sample = np.random.binomial(1, p)
+            output[j//row_length][j%row_length] = sample
+        subplot = generated_samples.add_subplot(plot_size, plot_size, i+1)
+        subplot.imshow(output,cmap='gray')
+        subplot.axis('off')
+    plt.show()
+
+generate_samples(25)
+
+auto_encode(64)
