f2e03417e1df1ce9e798daa9e6c0d3b011562747
Emma Alexandrov <ealexandrov@vassar.edu>
2019-06-14 16:44:08 -0400

    Organized functions

--- MADE/KerasImplementation/made.py
+++ MADE/KerasImplementation/made.py
@@ -10,6 +10,7 @@ from MADE.KerasImplementation.layers import MaskedDense
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import generators
 
 # for now, we'll use MNIST as our dataset
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
@@ -18,14 +19,6 @@ import math
 x_test = np.digitize(x_test, [0, 32]) - 1
 x_train = np.digitize(x_train, [0, 32]) - 1
 
-# mnist_samples = plt.figure(figsize=(5,5), facecolor='#ffffff')
-# for i in range(64):
-#     image = x_train[np.random.randint(0,x_train.shape[0]+1)]
-#     subplot = mnist_samples.add_subplot(math.sqrt(64), math.sqrt(64), i+1)
-#     subplot.imshow(image,cmap='gray')
-#     subplot.axis('off')
-# plt.show()
-
 # network parameters
 batch_size = 100
 num_epochs = 1
@@ -33,7 +26,7 @@ learning_rate = 0.01
 epsilon = 0.000001
 num_masks = 10
 hidden_layers = 2
-hidden_units = 150
+hidden_units = 1000
 features = 784
 
 batches_per_epoch = math.ceil(x_train.shape[0] / batch_size)
@@ -41,31 +34,34 @@ remainder_batch_size = x_train.shape[0] - ((batches_per_epoch-1) * batch_size)
 total_batches = batches_per_epoch * num_epochs
 
 # generate an array of mask matrices
-
-
-def gen_masks(num_masks, features, hidden_layers, hidden_units):
+def gen_masks(num_masks, features, hidden_layers, hidden_units, 
+              classes=None):
 
     all_masks = []
+    if classes is not None:
+        label_indices = []
+        for i in range(classes):
+            label_indices.append(0)
     first_input_indices = []
-    for j in range(features):
-        first_input_indices.append(j+1)
-    input_indices = []
-    input_dim = hidden_units
+    for i in range(features):
+        first_input_indices.append(i+1)
     for m in range(num_masks):
         masks = []
         input_indices = []
         # generate a mask for each hidden layer
         for i in range(hidden_layers):
             if len(input_indices) == 0:
-                input_dim = features
                 input_indices = first_input_indices
                 np.random.shuffle(input_indices)
+                if classes is not None:
+                    input_indices = label_indices + input_indices
             # we must generate a list of node indices for this layer
             layer_indices = []
             for j in range(hidden_units):
                 layer_indices.append(np.random.randint(min(input_indices),
                                      features - 1))
-            mask = np.zeros((input_dim, hidden_units), dtype=np.float32)
+            mask = np.zeros((len(input_indices), len(layer_indices)), 
+                            dtype=np.float32)
             # populate mask with appropriate values
             for j in range(len(input_indices)):  # every layer node
                 for k in range(len(layer_indices)):  # every input node
@@ -74,7 +70,6 @@ def gen_masks(num_masks, features, hidden_layers, hidden_units):
             mask = tf.convert_to_tensor(mask, dtype=tf.float32)
             masks.append(mask)
             input_indices = layer_indices
-            input_dim = len(input_indices)
         # generate output layer masks
         output_mask = np.zeros((hidden_units, features), dtype=np.float32)
         for j in range(len(input_indices)):
@@ -92,114 +87,6 @@ def gen_masks(num_masks, features, hidden_layers, hidden_units):
         masks.append(direct_mask)
         all_masks.append(masks)
     return all_masks
-
-# visualize auto-encoding and generative capacities
-# auto-encoding
-
-
-def auto_encode(model, num_samples, fname=None):
-
-    # visualize inputs
-    original_images = plt.figure(figsize=(5, 5), facecolor='#ffffff')
-    # visualize auto-encodings
-    output_images = plt.figure(figsize=(5, 5), facecolor='#ffffff')
-    for i in range(num_samples):
-        proto_input = x_test[np.random.randint(0, x_test.shape[0]+1)]
-        subplot = original_images.add_subplot(math.sqrt(num_samples),
-                                              math.sqrt(num_samples), i+1)
-        subplot.imshow(proto_input, cmap='gray')
-        subplot.axis('off')
-        input = np.empty((1, 28, 28))
-        input[0] = proto_input
-        output = model.predict(input, batch_size=1)
-        subplot = output_images.add_subplot(math.sqrt(num_samples),
-                                            math.sqrt(num_samples), i+1)
-        subplot.imshow(output[0], cmap='gray')
-        subplot.axis('off')
-    if fname is None:
-        plt.show()
-    if fname is not None:
-        plt.savefig(fname)
-
-# novel sample generation
-
-
-def zoom_generation(start, end, step):
-
-    plots = math.ceil(((end - start) / step) + 1)
-    plot_size = math.ceil(math.sqrt(plots))
-    noise = np.random.binomial(1, 0.5, size=(1, 28, 28))
-    row_length = noise.shape[1]
-    input = noise[0]
-    history_image = plt.figure(figsize=(10, 10), facecolor='#ffffff')
-    subplot = history_image.add_subplot(plot_size, plot_size, 1)
-    subplot.imshow(input, cmap='gray')
-    subplot.axis('off')
-    for i in range(start, end + 1):
-        output = made.predict(noise, batch_size=1)
-        p = output[0][i//row_length][i % row_length]
-        sample = np.random.binomial(1, p)
-        output[0][i//row_length][i % row_length] = sample
-        if i % step == 0:
-            plot_number = ((end - i) // step) + 1
-            subplot = history_image.add_subplot(plot_size,
-                                                plot_size, plot_number)
-            subplot.imshow(output[0], cmap='gray')
-            subplot.axis('off')
-    plt.show()
-
-
-def generate_sample_with_history(noise_parameter):
-
-    noise = np.random.binomial(1, noise_parameter, size=(1, 28, 28))
-    row_length = noise.shape[1]
-    input = noise[0]
-    plt.figure(figsize=(5, 5))
-    plt.imshow(input, cmap='gray')
-    plt.show()
-
-    output = np.zeros(input.shape, dtype=np.float32)
-    history_image = plt.figure(figsize=(8, 8), facecolor='#ffffff')
-    for i in range(1, len(input.flatten())):
-        noise = made.predict(noise, batch_size=1)
-        p = noise[0][i//row_length][i % row_length]
-        sample = np.random.binomial(1, p)
-        output[i//row_length][i % row_length] = sample
-        if i % 16 == 0:
-            plot_number = i // 16 + 1
-            subplot = history_image.add_subplot(7, 7, plot_number)
-            subplot.imshow(output, cmap='gray')
-            subplot.axis('off')
-    plt.show()
-
-    # visualize output
-    plt.figure(figsize=(5, 5))
-    plt.imshow(output, cmap='gray')
-    plt.show()
-
-
-def generate_samples(model, num_samples, fname=None):
-
-    noise_parameter = np.random.rand()
-    plot_size = math.ceil(math.sqrt(num_samples))
-    generated_samples = plt.figure(figsize=(10, 10), facecolor='#ffffff')
-    for i in range(num_samples):
-        noise = np.random.binomial(1, noise_parameter, size=(1, 28, 28))
-        output = np.zeros(noise[0].shape, dtype=np.float32)
-        row_length = noise.shape[1]
-        for j in range(1, len(noise.flatten())):
-            noise = model.predict(noise, batch_size=1)
-            p = noise[0][j // row_length][j % row_length]
-            sample = np.random.binomial(1, p)
-            noise[0][j // row_length][j % row_length] = p
-            output[j // row_length][j % row_length] = sample
-        subplot = generated_samples.add_subplot(plot_size, plot_size, i+1)
-        subplot.imshow(output, cmap='gray')
-        subplot.axis('off')
-    if fname is None:
-        plt.show()
-    if fname is not None:
-        plt.savefig(fname)
 
 # generate set of masks
 masks = gen_masks(num_masks, features, hidden_layers, hidden_units)
@@ -225,6 +112,7 @@ for i in range(num_epochs):
     # prepare batched data to feed to network
     data = []
     while len(data) < batches_per_epoch:
+        print('Batching data for epoch ' + str(i)+ 'of ' + str(num_epochs) + '.')
         np.random.shuffle(x_train)
         if len(data) * batch_size + batch_size > x_train.shape[0]:
             batch = x_train[len(data) * batch_size:]
@@ -243,7 +131,6 @@ for i in range(num_epochs):
         # take weights from MADE
         weights = made.get_weights()
         tf.keras.backend.clear_session()
-        inputs = tf.keras.Input(shape=(28, 28))
         # remake network with next mask set
         inputs = tf.keras.Input(shape=(28, 28))
         flatten = tf.keras.layers.Flatten()(inputs)
